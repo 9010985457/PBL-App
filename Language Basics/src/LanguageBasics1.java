8844aef79bf2c8406dcfe8afdcadf5973574b02d
Ashwath Nanda <ashwath5897@gmail.com>
2019-03-26 23:17:47 +0530

    Updated Directory

---
+++ Language Basics/src/LanguageBasics1.java
@@ -0,0 +1,7 @@
+public class LanguageBasics1 {
+    public static void main(String args[]) {
+        String s = args[0];
+        String s1 = args[1];
+        System.out.println(s+" Technologies "+s1);
+    }
+}
