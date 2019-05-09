f4345de8c84dacaa3379c6d5a0ed404e6061835f
Ashwath Nanda <ashwath5897@gmail.com>
2019-05-09 17:05:57 +0530

    Added HashSet

--- Collection/List/list1.java
+++ Collection/List/list1.java
@@ -4,17 +4,17 @@ public class list1 {
 	public static void main(String[] args) {
 		ArrayList<String> arr = new ArrayList<String>();
 		arr.add("January");
-    arr.add("Febuary");
-    arr.add("March");
-    arr.add("April");
-    arr.add("May");
-    arr.add("June");
-    arr.add("July");
-    arr.add("August");
-    arr.add("September");
-    arr.add("October");
-    arr.add("November");
-    arr.add("December");
+		arr.add("Febuary");
+		arr.add("March");
+		arr.add("April");
+		arr.add("May");
+		arr.add("June");
+		arr.add("July");
+		arr.add("August");
+		arr.add("September");
+		arr.add("October");
+		arr.add("November");
+		arr.add("December");
 
   Iterator<String> it = arr.iterator();
 		while(it.hasNext()){
