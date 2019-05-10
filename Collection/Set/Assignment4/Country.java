3385e7d7ba5dde125ca41aff8190da510b980c2b
Ashwath Nanda <ashwath5897@gmail.com>
2019-05-10 12:03:42 +0530

    Sets assignment added

--- Collection/Set/Assignment4/Country.java
+++ Collection/Set/Assignment4/Country.java
@@ -1,14 +1,16 @@
 import java.util.*;
 
 public class Country {
-	HastSet<String> H1 = new HashSet<String>();
+	//HastSet<String> H1 = new HashSet<String>();
 	
-	public HashSet<string> saveCountryName(String CountryName) {
+	TreeSet<String> H1 = new TreeSet<String>();
+	
+	public TreeSet<string> saveCountryName(String CountryName) {
 		H1.add(CountryName);
 		return H1;
 	}
 	public String getCountry(String CountryName) {
-		Iterator it = H1.iterator();
+		Iterator<String> it = H1.iterator();
 		
 		while(it.hasNext()) {
 			if(it.next().equals(CountryName))
